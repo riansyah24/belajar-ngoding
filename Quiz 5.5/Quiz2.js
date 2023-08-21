0bd5e83e1c71061c765a27481f32538dd5e00b24
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2023-08-21 13:47:47 +0700

    Quiz 5.5 Quiz2.js Minggu pertama

---
+++ Quiz 5.5/Quiz2.js
@@ -0,0 +1,17 @@
+/*
+Soal 2
+1. Memecah kalimat dan menampilkan menjadi kata
+*/
+
+let word = "Wow Javascript is so cool"
+let firstWord = `${word[0]}${word[1]}${word[2]}`
+let secondWord = `${word[4]}${word[5]}${word[6]}${word[7]}${word[8]}${word[9]}${word[10]}${word[11]}${word[12]}${word[13]}`
+let thirdWord = `${word[15]}${word[16]}`
+let fourthWord = `${word[18]}${word[19]}`
+let fifthWord = `${word[21]}${word[22]}${word[23]}${word[24]}`
+
+console.log(firstWord)
+console.log(secondWord)
+console.log(thirdWord)
+console.log(fourthWord)
+console.log(fifthWord)
