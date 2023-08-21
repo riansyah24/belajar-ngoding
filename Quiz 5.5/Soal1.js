fffb6e67e0070c41413d050b5f9917f44f4f61e0
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2023-08-21 13:45:31 +0700

    Quiz 5.5 Soal1 Minggu pertama

---
+++ Quiz 5.5/Soal1.js
@@ -0,0 +1,15 @@
+/*
+Soal 1
+1. Disediakan variabel yg berisi string
+2. Harus disatukan menjadi kalimat 
+*/
+
+let word1 = "Javascript"
+let second = "is"
+let third = "awesome"
+let fourth = "and"
+let fifth = "I"
+let sixth = "love"
+let seventh = "it"
+
+console.log(word1 + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh)￼Enter
