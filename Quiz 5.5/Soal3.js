8c2e47a9f64d895f1be87f2e221a3b92a163c8fa
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2023-08-21 13:49:17 +0700

    Quiz 5.5 Soal3.js Minggu pertama

---
+++ Quiz 5.5/Soal3.js
@@ -0,0 +1,17 @@
+/* 
+Soal 3
+Memecah kalimat menggunakan substring
+*/
+
+let word = "Wow Javascript is so cool"
+let firstWord = word.substring(0,3)
+let secondWord = word.substring(4,14)
+let thirdWord = word.substring(15,17)
+let fourthWord = word.substring(18,20)
+let fifthWord = word.substring(21,25)
+
+console.log(firstWord)
+console.log(secondWord)
+console.log(thirdWord)
+console.log(fourthWord)
+console.log(fifthWord)
