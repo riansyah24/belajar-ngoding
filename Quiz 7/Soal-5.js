d0d1a1d2e48bc1dd47f9822f328fdf8f52a1938c
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2023-08-25 22:06:33 +0700

    Quiz 7 Soal-5.js Minggu pertama

---
+++ Quiz 7/Soal-5.js
@@ -0,0 +1,19 @@
+/*
+1. function balikKata yg menerima parameter 1 string
+2. Return kata yang dibalik
+*/
+
+function balikKata(kata) {
+  let panjang = kata.length
+  let result = ""
+  for (let i = 0;panjang-1>=i;panjang--) {
+    result+=kata[panjang-1]
+  }
+  return result
+}
+
+console.log(balikKata("Hello World and Coders"))
+console.log(balikKata("John Doe"))
+console.log(balikKata("I am bookworm"))
+console.log(balikKata("Coding is my hobby"))
+￼Enter
