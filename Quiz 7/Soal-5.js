818b2c012ce7d2f5178f6ca8f7e3ecca3ca9f554
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2023-08-25 23:45:19 +0700

    Quiz 7 Soal-5.js Minggu pertama

--- Quiz 7/Soal-5.js
+++ Quiz 7/Soal-5.js
@@ -14,5 +14,5 @@ function balikKata(kata) {
 
 console.log(balikKata("Hello World and Coders"))
 console.log(balikKata("John Doe"))
-console.log(balikKata("I am bookworm"))
+console.log(balikKata("I am a bookworm"))
 console.log(balikKata("Coding is my hobby"))
